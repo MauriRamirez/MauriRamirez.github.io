95c20d4551236c4ccba98bbe89a45cc0fdb8eb47
Mauricio Ramirez <mauri.ra99@gmail.com>
2025-08-05 11:46:37 -0300

    update

---
+++ assets/webgl/TheYkeer-WebGLBuild/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Lechuga Studios-The Ykeer-1.0.0";
+const contentToCache = [
+    "Build/TheYkeer-WebGLBuild.loader.js",
+    "Build/TheYkeer-WebGLBuild.framework.js",
+    "Build/TheYkeer-WebGLBuild.data",
+    "Build/TheYkeer-WebGLBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
